d5f361a7a2d7c0fcfa95c3871a21b0e4203544e5
unknown <kimgeladavis@gmail.com>
2020-09-11 11:40:02 -0500

    person person person

--- postgrator-config.js
+++ postgrator-config.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   "migrationDirectory": "migrations",
+  "validateChecksums": false,
   "driver": "pg",
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DB_URL
