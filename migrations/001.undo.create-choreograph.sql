c9f3e360ccfb0c2174d9e8f5e4a753b6bc8431ba
unknown <kimgeladavis@gmail.com>
2020-09-14 11:54:27 -0500

    playing with keys

--- migrations/001.undo.create-choreograph.sql
+++ migrations/001.undo.create-choreograph.sql
@@ -2,11 +2,20 @@ DROP TABLE IF EXISTS costume;
 
 DROP TABLE IF EXISTS piecesteps;
 
+ALTER TABLE piecesteps
+    DROP COLUMN users;
+
 DROP TABLE IF EXISTS assignedpieces;
+
+ALTER TABLE assignedpieces
+    DROP COLUMN users;
 
 DROP TABLE IF EXISTS pieceoptions;
  
 DROP TABLE IF EXISTS graphpasswords;
+
+ALTER TABLE graphpasswords
+    DROP COLUMN users;
 
 DROP TABLE IF EXISTS graphusers;
 
